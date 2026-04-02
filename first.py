54172de6231bbf0de733e58770aab10290a264e0
kseshkkkkaaa <katkova.ksu1210@yandex.ru>
2026-04-02 21:45:58 +0400

    add fib(n) iteratively

--- first.py
+++ first.py
@@ -1,7 +1,11 @@
 def fib(n):
-    if n == 1:
-        return 0
-    elif n == 2:
-        return 1
-    return fib(n-1)+fib(n-2)
-print(fib(10))
+    if n >= 1:
+        print(0)
+    if n >= 2:
+        print(1)
+    a, b = 0, 1
+    for i in range(2,n):
+        c = a + b
+        print(c)
+        a, b = b, c
+fib(10)
